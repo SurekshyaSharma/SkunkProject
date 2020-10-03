f535f68022142aa193920ba028257447003f4d4e
SurekshyaSharma <shar3415@stthomas.edu>
2020-10-02 20:11:24 -0500

    Created the TestPredictableDie.java file

---
+++ SkunkProject/test/skunk/domain/TestPredictableDie.java
@@ -0,0 +1,14 @@
+package skunk.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestPredictableDie {
+
+	@Test
+	void test_PD_1_2_3() {
+		fail("Not yet implemented");
+	}
+
+}
